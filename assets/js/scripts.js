62a6521cbb3c2c5d15bf1d5d6ead2062108382ee
Frank Diaz <franklin.diaz@mercadolibre.com>
2024-11-06 23:09:30 -0300

    feature: Add job update

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+function calculateAge(birthdate) {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('age').innerText = calculateAge('1998-04-04');
+});
